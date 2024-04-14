9a017537c6b6196beb94b99e82c85a9139a82370
unknown <floresangela1414@gmail.com>
2024-04-14 16:12:11 +0800

    Added BookSeeder and modified the DatabaseSeeder file

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,22 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\book>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
  */
 class BookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Book::class;
+
     public function definition(): array
     {
         return [
-            //
+            'isbn' => $this->faker->isbn13,
+            'title' => $this->faker->sentence(4),
+            'author' => $this->faker->name,
+            'description' => $this->faker->sentence(10),
+            'date_published' => $this->faker->date(),
         ];
     }
 }
--- database/migrations/2024_04_14_080407_create_books_table.php
+++ database/migrations/2024_04_14_080407_create_books_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('isbn')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->date('date_published');
             $table->timestamps();
         });
     }
